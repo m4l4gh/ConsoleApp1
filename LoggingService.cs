39e0eac249a0dd125a1fabe5a4967247f3efc458
m4l4gh <mario.lampl@gmail.com>
2019-04-02 15:43:15 +0200

    Logger

---
+++ LoggingService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class LoggingService
+    {
+        
+        /// <summary>
+        /// Soll einen Eintrag in die log.txt anhängen.
+        /// </summary>
+        /// <param name="tolog"></param>
+        public void Log(string tolog)
+        {
+            var path = (string)Environment.GetEnvironmentVariables()["APPDATA"];
+            var folder = Path.Combine(path, "myapp");
+            Directory.CreateDirectory(folder);
+            var logfile = Path.Combine(folder, "log.txt");
+            using (var sw = new StreamWriter(logfile, append: true))
+            {
+                sw.WriteLine(tolog);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo1");
+            try
+            {
+                var logger = new LoggingService();
+            }
+            catch (UnauthorizedAccessException unae)
+            {
+                Console.WriteLine(unae.Message);
+            }
+            catch (Exception)
+            {
+
+                throw new DemoException("test");
+            }
+        }
+    }
+
+    public class DemoException:Exception
+    {
+        public DemoException(string msg)
+        {
+
         }
     }
 }
